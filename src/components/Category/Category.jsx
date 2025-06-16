330aa93707ad7becfe8ee13d5f38c39b9e17e364
vaibhav-shrivastava200804 <vaibhav2000804@gmail.com>
2025-06-16 11:03:19 +0530

    category card modified

--- src/components/Category/Category.jsx
+++ src/components/Category/Category.jsx
@@ -29,7 +29,7 @@ function Category() {
         <h1 className="text-3xl text-white bg-gradient-to-tr from-[#650D1B] via-[#DE0D30] to-[#632228] p-2 capitalize">{categoryId} &rarr;</h1>
         <div className="cards custom-scroll flex-1 flex flex-col sm:flex-row items-center overflow-x-auto gap-5 mb-1 p-1 shadow-2xl h-auto">
             {books.map((book) => (
-          <div key={book.key} className="flex flex-col gap-1 border-[white] border-[1px] rounded-md w-[240px] sm:w-[150px] p-1 flex-shrink-0 h-[400px] sm:h-[300px] overflow-clip hover:overflow-auto custom-scroll">
+          <div key={book.key} className="flex flex-col gap-1 border-[white] border-[1px] rounded-md w-[240px] sm:w-[200px] p-1 flex-shrink-0 h-[400px] sm:h-[400px] overflow-clip hover:overflow-auto custom-scroll">
             <img
               src={
                 book.cover_id
@@ -37,9 +37,9 @@ function Category() {
                   : "https://via.placeholder.com/150x200?text=No+Cover"
               }
               alt={book.title}
-              className="mb-2 w-[200px] h-[300px] sm:w-[150px] sm:h-[200px] border border-[#facc15] mt-1 mx-auto rounded-md cursor-pointer hover:scale-105 duration-200"
+              className="mb-2 w-[200px] h-[300px] sm:w-[210px] sm:h-[280px] border border-[#facc15] mt-1 mx-auto rounded-md cursor-pointer hover:scale-105 duration-200"
             />
-            <div className="detail flex flex-col gap-2 sm:gap-0 mx-1 my-2 overflow-hidden  hover:overflow-y-scroll custom-scroll">
+            <div className="detail flex flex-col sm:h-[100px] sm:gap-0 mx-1 my-2 overflow-hidden  hover:overflow-y-scroll custom-scroll">
               <h3 className="text-white ml-1"><b className='font-extrabold uppercase text-[white]'>Title:</b> <span className='font-medium'>{book.title}</span>
               </h3>
               <p className="text-white ml-1">
