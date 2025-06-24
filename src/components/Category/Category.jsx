72c53b3c9ada73528233534901cb0ea20442c655
vaibhav-shrivastava200804 <vaibhav2000804@gmail.com>
2025-06-24 23:59:49 +0530

    desktop category component design fixed

--- src/components/Category/Category.jsx
+++ src/components/Category/Category.jsx
@@ -34,9 +34,9 @@ function Category() {
   return (
     <>
     <div className="bg-gradient-to-tr from-[#650D1B] via-[#DE0D30] to-[#632228] mt-2">
-      <div className="text-white flex flex-col gap-2 h-[85vh] sm:h-auto">
+      <div className="text-white flex flex-col gap-2 h-[85vh] sm:h-[95vh] mx-auto">
         <h1 className="text-3xl text-white bg-gradient-to-tr from-[#650D1B] via-[#DE0D30] to-[#632228] p-2 capitalize">{categoryId} &rarr;</h1>
-        <div className="cards custom-scroll  flex flex-col sm:flex-row border overflow-y-scroll sm:flex-wrap items-center sm:justify-evenly sm:gap-10 gap-5 mb-1 p-1 shadow-2xl h-auto">
+        <div className="cards custom-scroll flex flex-col sm:flex-row overflow-y-scroll sm:flex-wrap items-center sm:justify-between sm:gap-10 gap-5 mb-1 p-1 shadow-2xl border rounded-lg h-auto w-[95%] mx-auto">
             {loading ? (
               <div className="flex mx-auto justify-center items-center w-[150px] h-[150px]">
                 <img src="https://i.gifer.com/ZZ5H.gif" alt="Loading..." className="w-20 h-20" />
@@ -45,16 +45,16 @@ function Category() {
               <div className="text-red-400 text-xl">{error}</div>
             ) : (
               books.map((book) => (
-          <div key={book.key} className="flex flex-col gap-1 border-[white] justify-evenly border-[1px] rounded-md w-[300px] sm:w-[300px] p-1 flex-shrink-0 h-[580px] sm:h-[500px] overflow-clip hover:overflow-auto custom-scroll">
-            <div className="w-[280px] h-[420px] sm:w-[280px] sm:h-[400px] flex items-center justify-center bg-white rounded-md overflow-hidden border-[#facc15] border mx-auto hover:scale-105 duration-300 cursor-pointer">
+          <div key={book.key} className="flex flex-col gap-1 border-[white] justify-between border-[1px] rounded-md w-[300px] sm:w-[320px] p-1 flex-shrink-0 h-[580px] sm:h-[500px] overflow-clip hover:overflow-auto custom-scroll">
+            <div className="w-[280px] h-[420px] sm:w-[300px] sm:h-auto flex items-center justify-center bg-white rounded-md overflow-hidden border-[#facc15] border mx-auto hover:scale-105 duration-300 cursor-pointer">
                 <img
                   src={
                     book.cover_id
                       ? `https://covers.openlibrary.org/b/id/${book.cover_id}-M.jpg`
                       : "https://via.placeholder.com/150x200?text=No+Cover"
                   }
-                  alt={book.title || "Book cover"}
-                  className="object-cover w-full h-full border-[#facc15]"
+                  alt={book.title || "Book cover" || "image not found"}
+                  className="w-[100%] h-[100%] border-[#facc15]"
                 />
               </div>
             <div className="detail flex flex-col h-[100px] sm:gap-0 mx-1 my-2 overflow-y-auto custom-scroll">
@@ -69,7 +69,7 @@ function Category() {
       )}
         </div>
       </div>
-      <div className="buttons flex justify-between p-2 text-2xl">
+      <div className="buttons flex justify-between p-2 text-2xl mx-auto w-[90%]">
       <button onClick={() => setOffset((prev) => Math.max(prev - limit, 0))} className="text-white p-2 rounded-md border-[#facc15] border-1 cursor-pointer hover:scale-105 duration-300">Previous</button>
       <button onClick={() => setOffset((prev) => prev + limit)} className="text-white p-2 rounded-md border-[#facc15] border-1 cursor-pointer hover:scale-105 duration-300">Next</button>
       </div>
